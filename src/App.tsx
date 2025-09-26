9ebfd3120182eb1ad76ffa361f220214b694cbb2
arthur-manske <arthur-manske@proton.me>
2025-09-25 21:43:50 -0300

    RM: Remove unused variables

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,7 @@ import Calendar from "./components/Calendar"
 import "./App.css"
 
 export default function App() {
-  const [selectedDate, setSelectedDate] = useState<Date | null>(null)
-
+//  const [selectedDate, setSelectedDate] = useState<Date | null>(null)
   return (
       <div className="app-grid">
       <header className="header"> Calendário </header>
